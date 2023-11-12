a14496315a4fe6fedea23465b41ae9dbde118b13
Tim Kadek Burmølle Hansen <timkbhansen@gmail.com>
2023-11-12 13:04:51 +0100

    My first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tims_CV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
